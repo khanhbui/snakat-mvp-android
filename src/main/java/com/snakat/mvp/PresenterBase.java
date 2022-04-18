1c59b0ec0b54e852d76a21a28d0de2c6f1ff9ee1
Khanh Bui <buihoangkhanh@gmail.com>
2022-04-18 16:08:46 +0700

    updated

--- src/main/java/com/snakat/mvp/PresenterBase.java
+++ src/main/java/com/snakat/mvp/PresenterBase.java
@@ -1,12 +1,13 @@
 package com.snakat.mvp;
 
 import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.disposables.Disposable;
 
 public abstract class PresenterBase<V extends Contract.View> implements Contract.Presenter<V> {
 
-    protected final CompositeDisposable mCompositeDisposable;
+    private final CompositeDisposable mCompositeDisposable;
 
-    protected V mView;
+    private V mView;
 
     public PresenterBase() {
         mCompositeDisposable = new CompositeDisposable();
@@ -21,5 +22,13 @@ public abstract class PresenterBase<V extends Contract.View> implements Contract
     public void onDetach() {
         mCompositeDisposable.dispose();
         mView = null;
+    }
+
+    protected V getView() {
+        return mView;
+    }
+
+    protected void addDisposable(Disposable disposable) {
+        mCompositeDisposable.add(disposable);
     }
 }
